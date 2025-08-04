49fe624ece76a3b9c13ecc9c39968b19579dcaa3
Preslav Petrov <145495481+PrP3trov@users.noreply.github.com>
2025-08-04 22:41:07 +0300

    .
    .

--- SportGearRental.Web/obj/Debug/net8.0/SportGearRental.Web.AssemblyInfo.cs
+++ SportGearRental.Web/obj/Debug/net8.0/SportGearRental.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportGearRental.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dc1d226a083cf243cc069c63170e273d8671638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d495c3c6ad2615ab673f65b53e64efc5621963")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportGearRental.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportGearRental.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
